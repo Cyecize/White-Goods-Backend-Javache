2ac29c02052348dae2ecef196605de7021c8bdf4
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-03-23 04:58:43 +0200

    Update summer and magic ioc container. Add .properties file Remove call to Summer to initialize the context, this is done automatically and calling it again causes objects to be instantiated for a second time.

--- src/main/java/com/cyecize/app/StartUp.java
+++ src/main/java/com/cyecize/app/StartUp.java
@@ -2,12 +2,11 @@ package com.cyecize.app;
 
 import com.cyecize.javache.embedded.JavacheEmbedded;
 import com.cyecize.summer.DispatcherSolet;
-import com.cyecize.summer.SummerAppRunner;
 
 public class StartUp extends DispatcherSolet {
 
     public StartUp() {
-        SummerAppRunner.run(StartUp.class);
+
     }
 
     public static void main(String[] args) {
