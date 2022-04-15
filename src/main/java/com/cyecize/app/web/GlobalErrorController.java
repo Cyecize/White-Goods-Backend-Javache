eafaca2ba7f3ceea3b500fa8e391b0cb90a08054
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-04-15 17:31:31 +0300

    Add exception handler for HttpNotFoundException. If index.html exists in the assets dir (FE is deployed), then forward the request to the front end.

--- src/main/java/com/cyecize/app/web/GlobalErrorController.java
+++ src/main/java/com/cyecize/app/web/GlobalErrorController.java
@@ -7,17 +7,50 @@ import com.cyecize.app.error.NotFoundApiException;
 import com.cyecize.http.HttpStatus;
 import com.cyecize.solet.HttpSoletRequest;
 import com.cyecize.solet.HttpSoletResponse;
+import com.cyecize.solet.SoletConfig;
+import com.cyecize.solet.SoletConstants;
+import com.cyecize.summer.areas.routing.exceptions.HttpNotFoundException;
 import com.cyecize.summer.areas.security.exceptions.UnauthorizedException;
 import com.cyecize.summer.areas.validation.exceptions.ConstraintValidationException;
 import com.cyecize.summer.areas.validation.interfaces.BindingResult;
 import com.cyecize.summer.areas.validation.models.FieldError;
 import com.cyecize.summer.common.annotations.Controller;
 import com.cyecize.summer.common.annotations.routing.ExceptionListener;
-
+import com.cyecize.summer.common.models.ModelAndView;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class GlobalErrorController {
+
+    private final SoletConfig soletConfig;
+
+    /**
+     * Logic for forwarding requests to the frontend. If index.html exists in the assets' dir, then
+     * the FE is deployed. In that case, then render the HTML content of that file regardless of the
+     * URL, FE will handle it. Otherwise, print an error.
+     */
+    @ExceptionListener(HttpNotFoundException.class)
+    public Object handleNotFoundException(HttpSoletResponse httpSoletResponse,
+            HttpNotFoundException ex) throws IOException {
+        final String assetsDir = this.soletConfig.getAttribute(SoletConstants.SOLET_CONFIG_ASSETS_DIR) + "";
+        if (Files.exists(Path.of(assetsDir + "/index.html"))) {
+            httpSoletResponse.setStatusCode(HttpStatus.OK);
+            final String fileContents = new String(
+                    Files.readAllBytes(Path.of(assetsDir + "/index.html")));
+
+            return new ModelAndView("index.html.twig", Map.of("data", fileContents));
+        }
+
+        httpSoletResponse.setStatusCode(HttpStatus.NOT_FOUND);
+        ex.printStackTrace();
+        return "Page not found!";
+    }
 
     @ExceptionListener(UnauthorizedException.class)
     public ErrorResponse handleUnauthorizedException(UnauthorizedException exception,
