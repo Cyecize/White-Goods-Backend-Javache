db0cade35e61088f5214a536bbd17f42f3022484
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-12-08 00:50:57 +0200

    Fix filter for type 'specific products all' listing all products from the cart as applicable.

--- src/main/java/com/cyecize/app/api/store/promotion/promotionfilters/DiscountSpecificProductsAllFilter.java
+++ src/main/java/com/cyecize/app/api/store/promotion/promotionfilters/DiscountSpecificProductsAllFilter.java
@@ -3,7 +3,6 @@ package com.cyecize.app.api.store.promotion.promotionfilters;
 import com.cyecize.app.api.store.cart.ShoppingCartItemDetailedDto;
 import com.cyecize.app.api.store.promotion.Promotion;
 import com.cyecize.app.api.store.promotion.PromotionProductItem;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,25 +11,40 @@ public class DiscountSpecificProductsAllFilter extends PromotionFilterBase {
 
     @Override
     protected boolean doTest(Promotion promotion, FilterPayloadDto payload) {
-        return !this.doFilterApplicableItems(promotion, payload.getItems()).isEmpty();
-    }
+        if (payload.getItems().isEmpty()) {
+            return false;
+        }
 
-    @Override
-    public List<ShoppingCartItemDetailedDto> doFilterApplicableItems(Promotion promotion,
-            List<ShoppingCartItemDetailedDto> items) {
-        final Map<Long, Integer> itemQtyMap = items.stream()
+        final Map<Long, Integer> itemQtyMap = payload.getItems().stream()
                 .collect(Collectors.toMap(
-                        item -> item.getProduct().getId(),
+                        o -> o.getProduct().getId(),
                         ShoppingCartItemDetailedDto::getQuantity
                 ));
 
         for (PromotionProductItem item : promotion.getProductItems()) {
             final Integer selectedQty = itemQtyMap.get(item.getProductId());
             if (selectedQty == null || selectedQty < item.getMinQuantity()) {
-                return new ArrayList<>();
+                return false;
             }
         }
 
-        return new ArrayList<>(items);
+        return true;
+    }
+
+    @Override
+    public List<ShoppingCartItemDetailedDto> doFilterApplicableItems(Promotion promotion,
+            List<ShoppingCartItemDetailedDto> items) {
+        final Map<Long, Integer> itemQtyMap = promotion.getProductItems().stream()
+                .collect(Collectors.toMap(
+                        PromotionProductItem::getProductId,
+                        PromotionProductItem::getMinQuantity
+                ));
+
+        return items.stream()
+                .filter(item -> {
+                    final Integer minQty = itemQtyMap.get(item.getProduct().getId());
+                    return minQty != null && item.getQuantity() >= minQty;
+                })
+                .collect(Collectors.toList());
     }
 }
